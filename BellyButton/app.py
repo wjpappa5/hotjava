f0fcd6237a8bad9da74ad0d8677eca41cab9a54e
Your Name <you@example.com>
2021-01-18 16:48:35 -0500

    Flask

---
+++ BellyButton/app.py
@@ -0,0 +1,22 @@
+import json
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+@app.route("/samples")
+def samples():
+    json_file = 'static/data/samples.json'
+    pry = open(json_file)
+    data = json.load(pry)
+    return data
+
+if __name__ == "__main__":
+    app.run(debug=True)
+
+
+
+#File Path for App: C:\Users\willj\hotjava\BellyButton
